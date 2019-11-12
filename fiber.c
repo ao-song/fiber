a5c9f609288816ff4768e9edf7a9c1e79dd350aa
Ao Song <ao.song@outlook.com>
2019-11-12 13:48:48 +0100

    impl

---
+++ fiber.c
@@ -0,0 +1,50 @@
+#include "fiber.h"
+
+#include <stdio.h>
+
+scheduler *fiber_open() {
+    scheduler *sched = malloc(sizeof(scheduler));
+    sched->fiber_num = 0;
+    sched->fiber_list = NULL;
+
+    return sched;
+}
+
+void fiber_close(scheduler *sched) {
+    assert(sched != NULL);
+
+    if (sched->fiber_list == NULL) {
+        free(sched);
+        sched = NULL;
+    }
+    else {
+        while (sched->fiber_list != NULL) {
+            fiber *tmp = sched->fiber_list;
+            sched->fiber_list = tmp->next;
+            free(tmp);            
+        }
+    }
+
+    free(sched);
+    sched = NULL;
+}
+
+int fiber_new(scheduler *sched, fiber_func func, void *arg) {
+    fiber *fb = malloc(sizeof(fiber));
+    fb->sched = sched;
+    fb->func = func;
+    fb->arg = arg;
+    fb->state = DETACHED;
+    fb->next = NULL;
+
+    if (sched->fiber_list == NULL) {
+        sched->fiber_list = fb;
+    }
+    else {
+        fiber *tmp = sched->fiber_list;
+        sched->fiber_list = fb;
+        fb->next = tmp;
+    }
+
+    return ++(sched->fiber_num);
+}
--- fiber.h
+++ fiber.h
@@ -1,5 +1,5 @@
-#ifndef FIBER_H
-#define FIBER_H
+#ifndef __FIBER_H__
+#define __FIBER_H__
 
 #if defined (__cplusplus)
 extern "C" {
@@ -9,18 +9,35 @@ extern "C" {
 
 #define STACK_SIZE (1024 * 1024)
 
-typedef void (*fiber_func)();
+typedef enum {
+    DETACHED,
+    ATTACHED,
+    RESUMED,
+    TERMINATED
+} fiber_state;
+
+typedef void (*fiber_func)(scheduler *sched, void *arg);
 
 typedef struct {
-    char stack[STACK_SIZE];
+    char stack[STACK_SIZE]; // copy-stack
     ucontext_t main;
+    int fiber_num;
+    fiber *fiber_list;
 } scheduler;
 
 typedef struct {
-    char *context;
+    char *ctx;
+    scheduler *sched;
+    fiber_func func;
+    void *arg;
+    fiber_state state;
+    char *stack;
+    fiber *next;
 } fiber;
 
-int fiber_new();
+scheduler *fiber_open();
+int fiber_close(scheduler *sched);
+int fiber_new(scheduler *sched, fiber_func func, void *arg);
 int fiber_resume();
 int fiber_yield();
 
